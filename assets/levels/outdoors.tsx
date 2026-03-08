7e9398180545dbd4d3c2afe12bbd390f9b5df3b5
scratchermatch <60678063+scratchermatch@users.noreply.github.com>
2026-03-08 01:02:38 -0700

    Moved and enabled plugin, created camera controller

---
+++ assets/levels/outdoors.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="outdoors" tilewidth="32" tileheight="32" tilecount="70" columns="14">
+ <image source="../sprites/Tiles.png" width="448" height="160"/>
+</tileset>
---
+++ scenes/camera_controller.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+@onready var map:TileMapLayer = %World
+
+const TILE_SIZE:int = 32
+
+func _ready() -> void:
+	apply_limits(calculate_map_rect(map))
+
+## Calculates the given map's bounding rect in global coordinates
+func calculate_map_rect(map:TileMapLayer) -> Rect2:
+	var tile_size_rect:Rect2 = map.get_used_rect()
+	tile_size_rect.size *= TILE_SIZE
+	return tile_size_rect
+## Sets the camera's limit variables to the given rectangle
+func apply_limits(limiting_rect: Rect2) -> void:
+	limit_left = limiting_rect.position.x
+	limit_right = limiting_rect.size.x
+	limit_top = limiting_rect.position.y
+	limit_bottom = limiting_rect.size.y
